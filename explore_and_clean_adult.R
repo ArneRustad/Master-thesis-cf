91da687bc574b32991f4bd5c6cc81694e4ef4310
Arne Rustad <ArneRustad@users.noreply.github.com>
2022-01-09 21:13:41 +0100

    Fixed preprocessing of dataset, used original train/test split from UCI machine learning repository

--- explore_and_clean_adult.R
+++ explore_and_clean_adult.R
@@ -1,30 +1,78 @@
 library(dplyr)
+library(tidyverse)
 library(data.table)
 library(ggplot2)
-df.adult = read.csv("Datasets//adult.data")#, strip.white = TRUE)
+library(stringr)
+library(xtable)
+df.adult.full = read.csv("Datasets//adult.csv", strip.white = TRUE)
+df.adult_train = read.table("Datasets//adult_train.data", sep = ",", strip.white = TRUE, col.names = colnames(df.adult.full))
+df.adult_train$train_test_split = "train"
+df.adult_test = read.table("Datasets//adult_test.data", sep = ",", strip.white = TRUE, col.names = colnames(df.adult.full))
+df.adult_test$income = str_remove(df.adult_test$income, "[:punct:]")
+df.adult_test$train_test_split = "test"
+df.adult = rbind(df.adult_train, df.adult_test)
 
+nrow(df.adult_train)
+nrow(df.adult_test)
+nrow(df.adult_train) + nrow(df.adult_test)
+nrow(df.adult.full)
 df.adult = df.adult %>% mutate(across(where(is.factor), as.character))
+
+for (col in 1:ncol(df.adult)) {
+  df.adult[, col] = ifelse(df.adult[, col] == "?", NA, df.adult[, col])
+}
+
+df.adult = na.omit(df.adult)
+nrow(df.adult)
+nrow(filter(df.adult, train_test_split == "train"))
+nrow(filter(df.adult, train_test_split == "test"))
+
+ncol(df.adult)
+
 View(df.adult)
 sapply(df.adult, class)
-sapply(df.adult[, sapply(df.adult, class) == "character"], table)
+list_discrete_classes_count = sapply(df.adult[, sapply(df.adult, class) == "character"], table)
+list_discrete_classes_count
+vec_classes_min = sapply(df.adult[, sapply(df.adult, class) %in% c("numeric", "integer")], min)
+vec_classes_max = sapply(df.adult[, sapply(df.adult, class) %in% c("numeric", "integer")], max)
+df_classes_count = data.frame("")
 
-ggplot(df.adult, aes(x = fnlwgt, y = ..density.., group = Salary, fill = Salary)) + geom_histogram(alpha = 0.5)
+df.variables = data.frame(Column = colnames(df.adult), Values = "", stringsAsFactors = FALSE)
+for (name in names(list_discrete_classes_count)) {
+  row = df.variables$Column == name
+  classes = list_discrete_classes_count[[name]]
+  temp_categories = ""
+  for (i in seq_along(classes)) {
+    if (i > 1) temp_categories = paste0(temp_categories, ", ")
+    temp_categories = paste0(temp_categories, names(classes)[i], " (", signif(classes[i] / nrow(df.adult) * 100, 2), "%)")
+  }
+  df.variables[row, "Values"] = temp_categories
+}
+for (i in seq_along(vec_classes_min)) {
+  row = df.variables$Column == names(vec_classes_min)[i]
+  df.variables[row, "Values"] = paste0("[", vec_classes_min[i], ", ", vec_classes_max[i], "]")
+}
+
+df.variables
+
+print(xtable(df.variables[df.variables$Column != "train_test_split",]), include.rownames = FALSE)
+
+ggplot(df.adult, aes(x = fnlwgt, y = ..density.., group = income, fill = income)) + geom_histogram(alpha = 0.5)
+
+table(df.adult$workclass, df.adult$income) / diag(table(df.adult$workclass, df.adult$workclass))
+
 
 
 df.adult.rv = df.adult
-#df.adult.rv$Race = ifelse(df.adult$Race == "White", "White", "Non-white")
-df.adult.rv$Country = ifelse(as.character(df.adult$Country) == "United-States", "US", "Non-US")
-df.adult.rv$Relationship = ifelse(df.adult$Relationship %in% c("Husband", "Wife"), "Husband/Wife", "Non-Husband/Wife")
-df.adult.rv$Occupation = df.adult$Occupation
-df.adult.rv$Marital.Status = ifelse(df.adult$Marital.Status %in% c("Married-AF-spouse", "Married-civ-spouse", "Married-spouse-absent"),
+df.adult.rv$Country = ifelse(as.character(df.adult$native.country) == "United-States", "US", "Non-US")
+#df.adult.rv$Relationship = ifelse(df.adult$Relationship %in% c("Husband", "Wife"), "Husband/Wife", "Non-Husband/Wife")
+df.adult.rv$Marital.Status = ifelse(df.adult$marital.status %in% c("Married-AF-spouse", "Married-civ-spouse", "Married-spouse-absent"),
                                     "Married", df.adult$Marital.Status)
-df.adult.rv$Workclass = ifelse(df.adult$Workclass == "Private", "Private",
-                               ifelse(df.adult$Workclass %in% c("Local-gov", "Federal-gov"), "Government",
-                                      "Other"))
-df.adult.rv$Education[df.adult.rv$Education %in%
+df.adult.rv$Workclass = ifelse(df.adult$workclass %in% c("Local-gov", "Federal-gov", "State-gov"), "Government", df.adult$workclass)
+df.adult.rv$Education[df.adult.rv$education %in%
                         c("10th", "11th", "12th", "1st-4th", "5th-6th", "7th-8th", "9th", "Preschool")] = "<=12th"
 
-df.adult.rv = df.adult.rv[, ! colnames(df.adult.rv) %in% c("Relationship", "Capital.Loss")]
+df.adult.rv = df.adult.rv[, ! colnames(df.adult.rv) %in% c("Relationship")]
 sapply(df.adult.rv[, sapply(df.adult.rv, class) == "character"], table)
 View(df.adult.rv)
 
