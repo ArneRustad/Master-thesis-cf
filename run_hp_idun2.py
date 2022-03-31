d33434996da04f973d25748ac5258d409b27ab7d
Arne Rustad <arner97@gmail.com>
2022-03-31 14:58:54 +0200

    hp idun

--- run_hp_idun2.py
+++ run_hp_idun2.py
@@ -25,35 +25,29 @@ data_train = pd.read_csv(dataset_train_path)
 data_test = pd.read_csv(dataset_test_path)
 discrete_columns = data_train.columns[data_train.dtypes == "object"]
 
-gan_algorithms_partial_vec = ["WGAN-GP", "WGAN-SGP"]
-wgan_lambda_partial_vec = [4, 6, 8, 10, 12, 14]
-gan_method_vec = [(wgan_lambda, algorithm)
-                  for wgan_lambda in wgan_lambda_partial_vec
-                  for algorithm in gan_algorithms_partial_vec
-                  ]
-n_synthetic_datasets_gan_method_comparison = 25
-n_epochs_gan_method = 100
+tf_data_use_vec = [False, True]
+n_synthetic_datasets_tf_data_use_comparison = 10
+n_epochs_tf_data_use = 100
 
-def create_tabGAN_for_gan_method(wgan_lambda, gan_method):
+def create_tabGAN_for_tf_data_use(tf_data_use):
     tg_qtr = TabGAN(data_train, n_critic = n_critic, opt_lr = opt_lr, adam_beta1 = adam_beta1,
                     quantile_transformation_int = True, quantile_rand_transformation = True,
-                    noise_discrete_unif_max = noise_discrete_unif_max, tf_data_use=True,
-                    gan_method=gan_method, wgan_lambda=wgan_lambda)
+                    noise_discrete_unif_max = noise_discrete_unif_max,
+                    tf_data_use=tf_data_use)
     return tg_qtr
 
 helpers.hp_tuning.generate_multiple_datasets_for_multiple_hyperparameters(
-    create_tabGAN_func=create_tabGAN_for_gan_method,
-    hyperparams_vec=gan_method_vec,
-    hyperparams_name = "gan_method",
-    hyperparams_subname=["lambda", "algorithm"],
-    n_epochs=n_epochs_gan_method,
+    create_tabGAN_func=create_tabGAN_for_tf_data_use,
+    hyperparams_vec=tf_data_use_vec,
+    n_epochs=n_epochs_tf_data_use,
     dataset_dir=const.dir.hyperparams_tuning(),
     batch_size=batch_size,
     subfolder="tabGAN-qtr",
-    n_synthetic_datasets=n_synthetic_datasets_gan_method_comparison,
+    n_synthetic_datasets=n_synthetic_datasets_tf_data_use_comparison,
     restart = True,
     redo_hyperparams_vec = [],
     plot_only_new_progress = True,
+    hyperparams_name = "tf_data_use",
     add_comparison_folder=True,
     overwrite_dataset=False,
     progress_bar_subprocess=True,
