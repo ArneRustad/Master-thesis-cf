473340a52533250c9f84a6213c952be08d738593
Arne Iver Rustad <arneir@markov.math.ntnu.no>
2022-03-31 17:05:02 +0200

    torch on idun testing

--- test_idun.py
+++ test_idun.py
@@ -1,7 +1,8 @@
 import time
 
 print("Starting script on Idun")
-for i in range(10):
-    print("Waiting 10 seconds")
-    time.sleep(10)
+# for i in range(10):
+#     print("Waiting 10 seconds")
+#     time.sleep(10)
+import torch
 print("Finished script on Idun")
