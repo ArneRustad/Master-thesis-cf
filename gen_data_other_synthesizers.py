34a7cdf1b3f317f4eaf9d89dca397a23d3caf073
Arne Rustad <arner97@gmail.com>
2022-04-01 14:58:55 +0200

    hp idun

--- gen_data_other_synthesizers.py
+++ gen_data_other_synthesizers.py
@@ -9,9 +9,17 @@ import torch
 
 print(f"GPU available: {torch.cuda.is_available()}")
 
-os.makedirs(const.dir.data_gen(), exist_ok=True)
+restart_all = True
+restart_specific = []
 
-data_test_tabfairgan = pd.read_csv(os.path.join(const.dir.data(), "df_adult_edited_test.csv"))
+n_epochs = 300
+batch_size = 500
+n_critics = 10
+n_synthetic_datasets = 10
+
+os.makedirs(const.dir.data_gen(), exist_ok=True)
+os.makedirs(const.dir.data_temp(), exist_ok=True)
+
 data_train_tabfairgan = pd.read_csv(os.path.join(const.dir.data(), "df_adult_edited_train.csv"))
 
 dataset_gen_dir_tabfairgan = os.path.join(const.dir.data_gen(), "tabFairGAN")
@@ -20,16 +28,10 @@ os.makedirs(dataset_gen_dir_tabfairgan, exist_ok=True)
 os.makedirs(dataset_gen_dir_tabfairgan_mod, exist_ok=True)
 
 size_of_fake_data_train = data_train_tabfairgan.shape[0]
-size_of_fake_data_test = data_test_tabfairgan.shape[0]
 
 data_train_tabfairgan.columns = data_train_tabfairgan.columns.str.replace(".", "_", regex = False)
 path_data_train_tabfairgan = os.path.join(const.dir.data_temp(), "df_adult_edited_train_tabFairGAN.csv")
 data_train_tabfairgan.to_csv(path_data_train_tabfairgan)
-
-n_epochs = 10
-batch_size = 500
-n_critics = 10
-n_synthetic_datasets = 10
 
 dim_hidden_layer = -1 # same as setting equal to input dim
 dim_latent_layer = -1 # same as setting equal to input dim
@@ -38,16 +40,16 @@ dim_latent_layer_mod = 128
 
 dataset_train_gen_path_tabfairgan_mod = os.path.join(dataset_gen_dir_tabfairgan, f"gen{0}.csv")
 
-
-
 print("-"*10 + "Generating tabFairGAN-mod datasets" + "-"*10)
-for i in tqdm(range(n_synthetic_datasets), desc = "Generated datasets", disable = False):
-    dataset_train_gen_path_tabfairgan_mod = os.path.join(dataset_gen_dir_tabfairgan_mod, f"gen{i}.csv")
-    subprocess.call(
-        f"python src\\tabFairGAN_nofair.py {path_data_train_tabfairgan} {n_epochs} {batch_size} "
-        f"{dataset_train_gen_path_tabfairgan_mod} --critic_repeat={n_critics} "
-        f"--dim_latent_layer={dim_latent_layer_mod} --dim_hidden_layer={dim_hidden_layer_mod}"
-    )
-    data_train_gen_mod = pd.read_csv(dataset_train_gen_path_tabfairgan_mod)
-    data_train_gen_mod.columns = data_train_gen_mod.columns.str.replace("_", ".")
-    data_train_gen_mod.to_csv(dataset_train_gen_path_tabfairgan_mod, index=True)
+with tqdm(range(n_synthetic_datasets), desc = "Generated datasets", disable = False) as pbar:
+    for i in range(n_synthetic_datasets):
+        print(pbar)
+        dataset_train_gen_path_tabfairgan_mod = os.path.join(dataset_gen_dir_tabfairgan_mod, f"gen{i}.csv")
+        subprocess.call(
+            f"python {os.path.join('src', 'TabFairGAN_nofair.py')} {path_data_train_tabfairgan} {n_epochs}"
+            f" {batch_size} {dataset_train_gen_path_tabfairgan_mod} --critic_repeat={n_critics}"
+            f" --dim_latent_layer={dim_latent_layer_mod} --dim_hidden_layer={dim_hidden_layer_mod}".split()
+        )
+        data_train_gen_mod = pd.read_csv(dataset_train_gen_path_tabfairgan_mod)
+        data_train_gen_mod.columns = data_train_gen_mod.columns.str.replace("_", ".")
+        data_train_gen_mod.to_csv(dataset_train_gen_path_tabfairgan_mod, index=True)
