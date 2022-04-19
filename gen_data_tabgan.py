3543d3c0efeaac879c17c6a65616a5056e0647f0
Arne Rustad <arner97@gmail.com>
2022-04-19 12:55:48 +0200

    hp idun

--- gen_data_tabgan.py
+++ gen_data_tabgan.py
@@ -19,18 +19,25 @@ noise_discrete_unif_max = 0
 n_synthetic_datasets = 25
 jit_compile = False
 
-def gen_datasets_tabgan(data_train, quantile_transformation=False, quantile_transformation_randomized=False):
-    method_name = "tabGAN"
+def gen_datasets_tabgan(data_train, quantile_transformation=False, quantile_transformation_randomized=False,
+                        ctgan=False, ctgan_log_freq=True):
+    method_name = ""
+    if ctgan:
+        method_name += "c"
+    method_name += "tabGAN"
     if quantile_transformation:
         method_name += "-qt"
         if quantile_transformation_randomized:
             method_name += "r"
+    if ctgan and not ctgan_log_freq:
+        method_name += "-log_freq=False"
     print_header_method(method_name)
 
     tg = TabGAN(data_train, n_critic=n_critic, opt_lr=opt_lr, adam_beta1=adam_beta1,
                 quantile_transformation_int=quantile_transformation,
                 quantile_rand_transformation=quantile_transformation_randomized,
-                noise_discrete_unif_max=noise_discrete_unif_max, jit_compile=jit_compile)
+                noise_discrete_unif_max=noise_discrete_unif_max, jit_compile=jit_compile,
+                ctgan=True, ctgan_log_frequency=ctgan_log_freq, tf_data_use=not ctgan)
 
     helpers.generate_multiple_datasets(tg, const.dir.data_gen(), n_synthetic_datasets, n_epochs, subfolder=method_name,
                                        batch_size=batch_size, overwrite_dataset=False, progress_bar_dataset=False)
@@ -38,4 +45,8 @@ def gen_datasets_tabgan(data_train, quantile_transformation=False, quantile_tran
 gen_datasets_tabgan(data_train, quantile_transformation=False, quantile_transformation_randomized=False)
 gen_datasets_tabgan(data_train, quantile_transformation=True, quantile_transformation_randomized=False)
 gen_datasets_tabgan(data_train, quantile_transformation=True, quantile_transformation_randomized=True)
+gen_datasets_tabgan(data_train, quantile_transformation=True, quantile_transformation_randomized=True,
+                    ctgan=True, ctgan_log_freq=True)
+gen_datasets_tabgan(data_train, quantile_transformation=True, quantile_transformation_randomized=True,
+                    ctgan=True, ctgan_log_freq=False)
 
