53f26438f46fd6431583ab36a7874a945dfbff23
Arne Iver Rustad <arneir@markov.math.ntnu.no>
2022-04-27 19:05:31 +0200

    Fix ctgan binomial loss

--- tabGAN/tabGAN.py
+++ tabGAN/tabGAN.py
@@ -858,7 +858,16 @@ class TabGAN:
                                                 gen_data_discrete=gen_data_discrete,
                                                 queries=queries)
             if self.ctgan and self.ctgan_binomial_loss:
-                loss_gen -= tf.reduce_mean(tf.math.log(tf.reduce_sum(queries * gen_data_discrete, axis=1)))
+                # tf.print(tf.reduce_sum(queries * gen_data_discrete, axis=1),
+                #          "1 - max_val:", 1 - tf.math.reduce_max(tf.reduce_sum(queries * gen_data_discrete, axis=1)),
+                #          tf.math.log(tf.reduce_sum(queries * gen_data_discrete, axis=1)),
+                #          "min_val:", tf.math.reduce_min(tf.math.log(tf.reduce_sum(queries * gen_data_discrete, axis=1))),
+                #          "total_loss:", tf.reduce_mean(tf.math.log(tf.reduce_sum(queries * gen_data_discrete, axis=1)))
+                #          )
+                #loss_gen -= tf.reduce_mean(tf.math.log(tf.reduce_sum(queries * gen_data_discrete, axis=1)))
+                vec_query_closeness = tf.minimum(tf.reduce_sum(queries * gen_data_discrete, axis=1), 0.99)
+                loss_gen -= tf.reduce_mean(tf.math.log(vec_query_closeness))
+
 
         gradients_of_generator = gen_tape.gradient(loss_gen, self.generator.trainable_variables)
         self.generator_optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
