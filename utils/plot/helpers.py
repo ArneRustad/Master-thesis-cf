ba0510dc4314b5bb1f881be1097c85a42a7725f9
Arne Rustad <arner97@gmail.com>
2022-06-16 15:39:32 +0200

    update to map str to color function

--- utils/plot/helpers.py
+++ utils/plot/helpers.py
@@ -1,9 +1,14 @@
 import matplotlib
 import numpy as np
+import matplotlib.pyplot as plt
 
 def _map_str_to_color(vec, color_map = "viridis"):
-    cmap = matplotlib.cm.get_cmap(color_map)
     vec_set = set(vec)
-    colors = cmap(np.linspace(0, 1, len(vec_set)))
-    clr_map = {string : colors[i] for i, string in enumerate(vec_set)}
+    if color_map is None:
+        fig, ax = plt.subplots()
+        colors = [next(ax._get_lines.prop_cycler)['color'] for i in range(len(vec_set))]
+    else:
+        cmap = matplotlib.cm.get_cmap(color_map)
+        colors = cmap(np.linspace(0, 1, len(vec_set)))
+    clr_map = {string: colors[i] for i, string in enumerate(sorted(vec_set))}
     return([clr_map[s] for s in vec])
