078ba93021d48e9dcbdc1ebd2c9aaea6e5babc71
Arne Rustad <arner97@gmail.com>
2022-06-26 05:15:47 +0200

    small change to compare_marginal_hists, must be reversed later

--- helpers/compare/compare_marginal_hists.py
+++ helpers/compare/compare_marginal_hists.py
@@ -9,6 +9,10 @@ def compare_hist_real_vs_generated(model, other_gen_dataset = None, n_img_horiz 
         gen_data = model.generate_data()
     else:
         gen_data = other_gen_dataset
+    model.data.loc[:, "race"] = np.where(model.data.loc[:, "race"] == "Amer-Indian-Eskimo",
+                                         "Native-American-Inuit", model.data.loc[:, "race"])
+    gen_data.data.loc[:, "race"] = np.where(gen_data.data.loc[:, "race"] == "Amer-Indian-Eskimo",
+                                            "Native-American-Inuit", model.data.loc[:, "race"])
     n_img_vert = (model.n_columns-1) // n_img_horiz + 1
     if(figsize == None):
         figsize = (15, 3*n_img_vert)
