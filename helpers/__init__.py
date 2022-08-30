6a04d894ca85b8a6a1dd0a09e982fdcf32179bfd
Arne Rustad <arner97@gmail.com>
2022-08-30 16:09:25 +0200

    Small restructuring of files

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,3 +1,3 @@
 # from . import compare, hp_tuning, eval, nmi_matrix, comparison
 from .tabgan_gen_multiple_datasets import generate_multiple_datasets
-from .measure_synthesizer_speed import measure_synthesizer_speed
+from helpers.eval.measure_synthesizer_speed import measure_synthesizer_speed
