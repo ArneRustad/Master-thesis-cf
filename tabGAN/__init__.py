672df01f6590ac59fc534d2b07866173e4e95c2b
Arne Rustad <arner97@gmail.com>
2022-08-30 13:52:50 +0200

    Restructured and tidyed u up the github project

--- tabGAN/__init__.py
+++ tabGAN/__init__.py
@@ -1,4 +1,2 @@
 from .tabGAN import TabGAN
-from .tabGAN2 import TabGAN2
 from .tabGAN_cf import TabGANcf
-from .tabGAN_cf_mc import TabGANcfmc
--- test_idun.py
+++
@@ -1,8 +0,0 @@
-import time
-
-print("Starting script on Idun")
-# for i in range(10):
-#     print("Waiting 10 seconds")
-#     time.sleep(10)
-import torch
-print("Finished script on Idun")
